bf3312ce4a7baa20a17b78d1d669bd31233c76e4
yoyzoA <yorgoassal0@gmail.com>
2024-04-12 00:09:37 +0300

    Chat commit TODO: registration

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import pgFnf from "./components/error/pgFnf";
 
 import { AuthProvider } from "./contexts/authContext";
 import { useRoutes } from "react-router-dom";
+import ChatHome from "../src/components/chat/ChatHome";
 
 function App() {
   
@@ -27,6 +28,10 @@ function App() {
     {
       path: "/home",
       element: <Home />,
+    },
+    {
+      path: "/chat",
+      element: <ChatHome />,
     },
   ];
 
--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -2,26 +2,65 @@ import React, { useState } from 'react'
 import { Navigate, Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../contexts/authContext'
 import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
+import Add from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
-
-    const navigate = useNavigate()
-
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [confirmPassword, setconfirmPassword] = useState('')
-    const [isRegistering, setIsRegistering] = useState(false)
-    const [errorMessage, setErrorMessage] = useState('')
-
-    const { userLoggedIn } = useAuth()
-
+    const [err, setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+  
     const onSubmit = async (e) => {
-        e.preventDefault()
-        if(!isRegistering) {
-            setIsRegistering(true)
-            await doCreateUserWithEmailAndPassword(email, password)
-        }
-    }
+      setLoading(true);
+      e.preventDefault();
+      const displayName = e.target[0].value;
+      const email = e.target[1].value;
+      const password = e.target[2].value;
+      const file = e.target[3].files[0];
+
+    try {
+        //Create user
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+  
+        //Create a unique image name
+        const date = new Date().getTime();
+        const storageRef = ref(storage, `${displayName + date}`);
+  
+        await uploadBytesResumable(storageRef, file).then(() => {
+          getDownloadURL(storageRef).then(async (downloadURL) => {
+            try {
+              //Update profile
+              await updateProfile(res.user, {
+                displayName,
+                photoURL: downloadURL,
+              });
+              //create user on firestore
+              await setDoc(doc(db, "users", res.user.uid), {
+                uid: res.user.uid,
+                displayName,
+                email,
+                photoURL: downloadURL,
+              });
+  
+              //create empty user chats on firestore
+              await setDoc(doc(db, "userChats", res.user.uid), {});
+              navigate("/");
+            } catch (err) {
+              console.log(err);
+              setErr(true);
+              setLoading(false);
+            }
+          });
+        });
+      } catch (err) {
+        setErr(true);
+        setLoading(false);
+      }
+    };
+    
 
     return (
         <>
@@ -39,6 +78,19 @@ const Register = () => {
                         onSubmit={onSubmit}
                         className="space-y-4"
                     >
+                        <div>
+                            <label className="text-sm text-gray-600 font-bold">
+                                username
+                            </label>
+                            <input
+                                type="username"
+                                autoComplete='username'
+                                required
+                                value={displayName} onChange={(e) => { setUsername(e.target.value) }}
+                                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+                            />
+                        </div>
+
                         <div>
                             <label className="text-sm text-gray-600 font-bold">
                                 Email
@@ -80,16 +132,23 @@ const Register = () => {
                             />
                         </div>
 
-                        {errorMessage && (
-                            <span className='text-red-600 font-bold'>{errorMessage}</span>
+                        <div>
+                            <label htmlFor="file">
+                            <img src={Add} alt="" />
+                            <span>Add an avatar</span>
+                            </label>
+                        </div>
+
+                        {err && (
+                            <span className='text-red-600 font-bold'>{err}</span>
                         )}
 
                         <button
                             type="submit"
-                            disabled={isRegistering}
-                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${isRegistering ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
+                            disabled={loading}
+                            className={`w-full px-4 py-2 text-white font-medium rounded-lg ${setLoading ? 'bg-gray-300 cursor-not-allowed' : 'bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300'}`}
                         >
-                            {isRegistering ? 'Signing Up...' : 'Sign Up'}
+                            {loading ? 'Signing Up...' : 'Sign Up'}
                         </button>
                         <div className="text-sm text-center">
                             Already have an account? {'   '}
